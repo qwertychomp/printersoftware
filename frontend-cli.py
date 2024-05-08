a345c11d262499a578930076fbd9f2b3ba094567
Sequoia Haynes <qwertychomp@github.com>
2024-05-08 12:00:20 -0700

    explain what every piece of damn code does

--- frontend-cli.py
+++ frontend-cli.py
@@ -1,14 +1,21 @@
-
+#import all the libraries, which provide premade functions 
+#(similiar to functions from math) for common tasks 
+#like reading images and manipulating lists
 import cv2
 from numpy import *
 import sys
 import minecraft
+#command-line arguments so the user can use 
+#settings other than the defaults if they want
 funnyArgs = sys.argv
 if(len(funnyArgs)<1):
     raise Exception("smartahh you need more arguments")
 if(len(funnyArgs)>1):
     raise Exception("smartahh you need less arguments")
 print(funnyArgs)
+#classes (objects that hold functions and value)
+
+#vector2 (a coordinate pair)
 class Vector2:
     def __init__(self, x, y) -> None:
         self.x = x
@@ -20,7 +27,9 @@ class Vector2:
     def __minus__(self, other):
         Vector2(self.x - other.x, self.y - other.y)
 
-
+#a matrix/grid object (like from math!) to store an image 
+#made of pixels (a bitmap image!) in a form that can easily be
+#manipulated through the built-in list functions
 class BlackAndWhiteImageMatrix:
     def __init__(self) -> None:
         self.size = Vector2(0, 0)
@@ -41,6 +50,7 @@ class BlackAndWhiteImageMatrix:
             self.matrix[pos.x][pos.y] = value
         else:
             raise Exception("imageMatrix is black and white only")
+#an object for storing an image made of lines as opposed to pixels (a vector image)
 class vectorMatrixThingy(BlackAndWhiteImageMatrix):
     def __init__(self) -> None:
         super().__init__()
@@ -56,12 +66,18 @@ class vectorMatrixThingy(BlackAndWhiteImageMatrix):
                 new_x.append(0)
 
             self.pointConnectionMatrix.append(new_x)
+#a function to see if, in a bitmap image, you can connect two points
+#by following black pixels on the image (i just started working on it)
 def isPathBetweenTwoPointsExistentOnGod(pos1:Vector2,pos2:Vector2,matrix:list):
     isResultFound = False
     stopAtYpoint = 0
-    if(pos1.y<)
+    if(pos1.y<pos2.y):
+        stopAtYpoint=1
+    elif(pos1.y>pos2.y):
+        stopAtYpoint=-1
     while(not isResultFound):
-
+        pass
+#this code executes all the stuff for the middle-end, which im still working on
 def funnyMiddleendThingyToDoStuff(leImage:BlackAndWhiteImageMatrix):
     level_of_detail = 50
     lePointImageTHingy = vectorMatrixThingy()
@@ -81,7 +97,8 @@ def funnyMiddleendThingyToDoStuff(leImage:BlackAndWhiteImageMatrix):
         lodTicker+=1
         if (lodTicker>level_of_detail):
             lodTicker=0
-
+#this takes a string (datatype for strings of characters) and converts
+#it into aBlackAndWhiteImageMatrix 
 def matrixFromImage(fileLocation:str)->BlackAndWhiteImageMatrix:
     newImage = cv2.imread(fileLocation, cv2.IMREAD_GRAYSCALE)
     newImageMatrix = BlackAndWhiteImageMatrix()
@@ -95,7 +112,8 @@ def matrixFromImage(fileLocation:str)->BlackAndWhiteImageMatrix:
                 newImageMatrix.set_pixel(Vector2(x, y), 0)
     newImageMatrix = generateEdgeMap(newImageMatrix)
     return newImageMatrix
-
+#simple code to take 9 pixels and see if the center one  has three
+#black pixels neighboring it
 def threeNeighborsCheck(list:List)->bool:
     incrementer = 0
     for x in range(len(list)):
@@ -104,6 +122,8 @@ def threeNeighborsCheck(list:List)->bool:
             if(incrementer == 3):
                 return True
     return False
+#take an image matrix and convert it to a new image matrix that 
+#only includes the pixels that are neighboring 3 or more white pixels
 def generateEdgeMap(imageMatrix:BlackAndWhiteImageMatrix)->BlackAndWhiteImageMatrix:
     newImageMatrix = BlackAndWhiteImageMatrix()
     newImageMatrix.create(imageMatrix.size.x, imageMatrix.size.y)
